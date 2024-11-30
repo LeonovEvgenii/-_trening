5b40c9098a16be914489524a13432bd3f61ebcc9
LeonovEvgenii <leonovevgenii2015@yandex.ru>
2024-11-30 23:53:04 +0500

    Типы данных

---
+++ data_type.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+    short a = 1;
+    int b = 1;
+    long c = 1;
+    unsigned int bb = 1;
+    signed int bbb = 1;
+    char d = 'q'; // важно именно одинарные кавычки
+    int number_system_16 = 0x1A;
+    int number_system_8 = 034;
+    int number_system_2 = 0b1010;
+    unsigned long literals_example = 0b1010;
+    std::cout << "hw\n";
+    return 0;
+}
