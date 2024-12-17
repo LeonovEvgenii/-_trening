7ff4a6ec6f07a3f34eb2c65f5beb44d19289cec6
LeonovEvgenii <leonovevgenii2015@yandex.ru>
2024-12-17 21:48:31 +0500

    3 задача 9 группы

--- promobot/9 Явное и неявное приведение типов/dynamic_cast.cpp
+++ promobot/9 Явное и неявное приведение типов/dynamic_cast.cpp
@@ -3,8 +3,54 @@
 
 #include <iostream>
 
+class A
+{
+    public:
+        virtual ~A() // причем обязателен виртуальный метод для dynamic_cast
+        {
+
+        }
+};
+
+class B: public A
+{
+    public:
+        B()
+        {
+
+        }
+
+};
+
+class C: public A
+{
+    public:
+        C()
+        {
+
+        }
+
+};
+
+
 int main()
 {
+    A *ptr_A = new B();
+
+    B *ptr_B = dynamic_cast<B*>(ptr_A);
+
+    if (ptr_B == nullptr)
+    {
+        std::cout << "приведение в указатель на B не удалось\n";
+    }
+
+    C *ptr_C = dynamic_cast<C*>(ptr_A);
+
+    if (ptr_C == nullptr)
+    {
+        std::cout << "приведение в указатель на C не удалось\n";
+    }
+
     std::cout << "hw\n";
     return 0;
 }
