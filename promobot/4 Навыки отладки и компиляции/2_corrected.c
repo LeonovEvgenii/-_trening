944c4dbe529d81b65b3235cb9bcf53f41ef9a1eb
Artem Smirnov <artsmi.vic@gmail.com>
2024-12-20 14:34:24 +0500

    Lesson 4 was reviewed

--- promobot/4 Навыки отладки и компиляции/2_corrected.c
+++ promobot/4 Навыки отладки и компиляции/2_corrected.c
@@ -3,10 +3,13 @@
 int main(){
     int var1 = 0;
     int var2 = 2;
+    //Q// когда маллок может вернуть NULL? почему нет проверки?
     char *buffer = malloc(sizeof(char) * 10);
+    //Q// чем отличается i++ от ++i с т.з. количества операций? почему?
     for(int i = 0 ; i < 10 ; i++){
         *(++buffer) = 'c';
     }
+    //Q//где выделяется память этой переменной? какой объем?
     char greeting[] = "Hello from losst\n";
-    printf("%s", greeting);
+    printf("%s", greeting); //Q// что будет если запустишь программу несколько раз? 
 }
--- promobot/4 Навыки отладки и компиляции/2_segmentation_fault.c
+++ promobot/4 Навыки отладки и компиляции/2_segmentation_fault.c
@@ -4,6 +4,7 @@ int main(){
     int var1 = 0;
     int var2 = 2;
     char *buffer = malloc(sizeof(char) * 10);
+    //Q// это нормальный практика цикла? как его можно прервать при условии неизменности строчки while(1)
     while(1){
         *(++buffer) = 'c';
     }
