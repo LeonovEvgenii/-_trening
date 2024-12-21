15b19226d24cfe7d8ad8311983ece997bc7a74a1
LeonovEvgenii <leonovevgenii2015@yandex.ru>
2024-12-21 21:18:23 +0500

    Добавил проверку на NULL

--- promobot/4 Навыки отладки и компиляции/2_segmentation_fault.c
+++ promobot/4 Навыки отладки и компиляции/2_segmentation_fault.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
     int var1 = 0;
     int var2 = 2;
     char *buffer = malloc(sizeof(char) * 10);
+
+    if (buffer == NULL)
+    {
+        return 0;
+    }
+
     //Q// это нормальный практика цикла? как его можно прервать при условии неизменности строчки while(1)
     while(1){
         *(++buffer) = 'c';
