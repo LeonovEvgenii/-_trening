36e46feba702da70a9cf8396a056eef19ebd048a
LeonovEvgenii <leonovevgenii2015@yandex.ru>
2024-12-21 21:20:17 +0500

    Добавил проверку на NULL

--- promobot/4 Навыки отладки и компиляции/2_corrected.c
+++ promobot/4 Навыки отладки и компиляции/2_corrected.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 
 int main(){
     int var1 = 0;
     int var2 = 2;
     //Q// когда маллок может вернуть NULL? почему нет проверки?
     char *buffer = malloc(sizeof(char) * 10);
+
+    if (buffer == NULL)
+    {
+        return 0;
+    }
+
     //Q// чем отличается i++ от ++i с т.з. количества операций? почему?
     for(int i = 0 ; i < 10 ; i++){
         *(++buffer) = 'c';
